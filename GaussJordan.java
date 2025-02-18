a6b1fbc59a2a429721d4ba1bee8539076e604ab6
Fiona Rowan <fmr2112@columbia.edu>
2025-02-18 08:06:04 -0500

    fix boolean statement, float instead of double

--- GaussJordan.java
+++ GaussJordan.java
@@ -9,12 +9,12 @@ public class GaussJordan {
 
 	public static void main(String args[]) {
 		final Scanner commandlineInput = new Scanner(System.in);
-		final double[][] augmentedMatrix;
+		final float[][] augmentedMatrix;
 		final int columns;
 		final int rows;
 
 		try {
-			final boolean invalidInput = args.length != 1 && !args[0].equals("example") && !args[0].equals("input");
+			final boolean invalidInput = args.length != 1 || !args[0].equals("example") && !args[0].equals("input");
 			if (invalidInput) {
 				System.out.println(
 						"Use \"example\" in commandline argument to see the Gauss Jordan Elimination algorithm in action with a given example.");
@@ -25,14 +25,14 @@ public class GaussJordan {
 
 			if (args[0].equals("example")) {
 				System.out.println("This program simulates example from p.14.");
-				augmentedMatrix = new double[][] {
+				augmentedMatrix = new float[][] {
 						{ 2, 4, -2, 2, 4, 2 },
 						{ 1, 2, -1, 2, 0, 4 },
 						{ 3, 6, -2, 1, 9, 1 },
 						{ 5, 10, -4, 5, 9, 9 }
 				};
 
-				// double array represents augmented matrix, from question 1.2.5
+				// float array represents augmented matrix, from question 1.2.5
 				rows = augmentedMatrix.length;
 				columns = augmentedMatrix[0].length;
 			} else {
@@ -40,7 +40,7 @@ public class GaussJordan {
 				columns = commandlineInput.nextInt();
 				System.out.println("How many rows?");
 				rows = commandlineInput.nextInt();
-				augmentedMatrix = new double[rows][columns];
+				augmentedMatrix = new float[rows][columns];
 				for (int i = 0; i < rows; i++) {
 					System.out.println("Type all numbers in row " + i + " separated by space");
 					for (int j = 0; j < columns; j++) {
@@ -57,12 +57,12 @@ public class GaussJordan {
 		gaussJordan(rows, columns, augmentedMatrix);
 	}
 
-	private static void gaussJordan(final int rows, final int columns, final double[][] augmentedMatrix) {
+	private static void gaussJordan(final int rows, final int columns, final float[][] augmentedMatrix) {
 		for (int i = 0; i < rows; i++) {
 			final int leadingColumn = getLeadingColumnForRow(columns, augmentedMatrix[i]);
 
 			if (leadingColumn != -1) {
-				final double leadingValue = augmentedMatrix[i][leadingColumn];
+				final float leadingValue = augmentedMatrix[i][leadingColumn];
 
 				// divide every value in row by lead coefficient
 				for (int j = 0; j < columns; j++) {
@@ -71,7 +71,7 @@ public class GaussJordan {
 
 				// zero out other values in this column for all other rows
 				for (int row = i + 1; row < rows; row++) {
-					final double val = augmentedMatrix[row][leadingColumn];
+					final float val = augmentedMatrix[row][leadingColumn];
 					if (val != 0) {
 						for (int j = 0; j < columns; j++) {
 							augmentedMatrix[row][j] = augmentedMatrix[row][j] - val * augmentedMatrix[i][j];
@@ -89,9 +89,9 @@ public class GaussJordan {
 		printMatrix(rows, columns, getEchelonForm(rows, columns, augmentedMatrix));
 	}
 
-	private static double[][] getEchelonForm(final int rows, final int columns, final double[][] augmentedMatrix) {
+	private static float[][] getEchelonForm(final int rows, final int columns, final float[][] augmentedMatrix) {
 		// swap rows
-		final double[][] solutionMatrix = new double[rows][columns];
+		final float[][] solutionMatrix = new float[rows][columns];
 		final boolean[] rowAdded = new boolean[rows];
 		int nextSolutionRow = 0;
 		for (int i = 0; i < columns; i++) {
@@ -107,11 +107,11 @@ public class GaussJordan {
 		return solutionMatrix;
 	}
 
-	private static int getLeadingColumnForRow(final int columns, final double[] augmentedRow) {
+	private static int getLeadingColumnForRow(final int columns, final float[] augmentedRow) {
 		int leadingColumn = -1;
 
 		for (int k = 0; k < columns; k++) {
-			final double value = augmentedRow[k];
+			final float value = augmentedRow[k];
 			if (value != 0) {
 				leadingColumn = k;
 				break;
@@ -120,7 +120,7 @@ public class GaussJordan {
 		return leadingColumn;
 	}
 
-	private static void printMatrix(final int rows, final int columns, final double[][] augmentedMatrix) {
+	private static void printMatrix(final int rows, final int columns, final float[][] augmentedMatrix) {
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
 				System.out.print(augmentedMatrix[i][j] + ", ");
